72ceed1f045b78cf9e3c31a98d64001cecd51793
Captain Nick Lucifer* <naryal2580@gmail.com>
2022-07-29 21:04:55 +0545

    Deploying mdbook to GitHub pages.

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Adding a worktree.
-if [[ -d /tmp/mdbook ]]; then
-  rm -rf /tmp/mdbook
-fi; git worktree add -f /tmp/mdbook
+if [[ -d /tmp/gh-pages ]]; then
+  rm -rf /tmp/gh-pages
+fi; git worktree add /tmp/gh-pages
 
 # Building the mdbook
 mdbook build
 
 # Copying all the built files to /tmp
-rm -rf /tmp/mdbook/* && cp -rp ./book/* /tmp/mdbook
+rm -rf /tmp/gh-pages/* && cp -rp ./book/* /tmp/gh-pages
 
 # Push all the built files to github.
-cd /tmp/mdbook
+cd /tmp/gh-pages
+git update-ref -d refs/heads/gh-pages
 git add -A
 git commit -m 'Deploying mdbook to GitHub pages.'
-git push origin gh-pages
-git update-ref -d refs/heads/gh-pages
+git push -f origin gh-pages
 cd -
 echo "done!"
